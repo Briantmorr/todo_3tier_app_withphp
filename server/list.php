1e9f98c2f9e2a7690b95dd1cee75909df35c1bda
Daniel Paschal <daniel.paschal@learningfuze.com>
2018-03-17 10:26:26 -0700

    added read and fixed typos    fixed typo in connect files    added initial read of database

--- server/list.php
+++ server/list.php
@@ -1,5 +1,14 @@
 <?php
-
 require_once( 'mysqlConnect.php');
+
+$query = "SELECT td.ID AS todoID, td.title, td.description, td.dueDate, td.owner, 
+tu.username AS todoOwner
+	FROM todoData AS td
+	JOIN todoUsers AS tu 
+		ON td.owner = tu.ID
+	WHERE td.status = 'active'";
+
+$result = mysqli_query($conn, $query);
+
 
 ?>
