476840f0bd26847cf6274daaa626382974311b76
Daniel Paschal <daniel.paschal@learningfuze.com>
2018-03-17 10:34:48 -0700

    completed read operation from DB

--- server/list.php
+++ server/list.php
@@ -10,5 +10,37 @@ tu.username AS todoOwner
 
 $result = mysqli_query($conn, $query);
 
+$output = [
+	'success' => false,
+	'errors' => [], 
+	'data' => []
+];
 
+if($result){  //if the query didn't screw up
+	if(mysqli_num_rows($result)>0){  //if there was data in the query result
+		while( $row = mysqli_fetch_assoc($result)){ //go through each one and grab the data
+			$output['data'][] = $row;  //push it into an array
+			$output['success'] = true;
+		}
+	} else {
+		$output['errors'][] = 'NO DATA AVAILABLE!';
+	}
+} else {
+	//array_push($output['errors'], 'Error in database query!');
+	$output['errors'][] = 'Error in database query!';
+}
+
+$json_output = json_encode($output);
+
+print($json_output);
 ?>
+
+
+
+
+
+
+
+
+
+
